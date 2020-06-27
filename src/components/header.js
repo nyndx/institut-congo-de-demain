64ddb229c38ba4c6d981c52f5194fd8992058d73
Max Ndundula <nynd_x@pm.me>
2020-06-27 00:33:39 -0400

    home page

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,65 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+const Header = ({ siteTitle }) => {
+  const { bgImg, logo } = useStaticQuery(graphql`
+    query MyQuery {
+      bgImg: file(relativePath: { eq: "annie-unsplash.jpg" }) {
+        sharp: childImageSharp {
+          fluid(fit: COVER, maxWidth: 2400) {
+            ...GatsbyImageSharpFluid_withWebp_tracedSVG
+          }
+        }
+      }
+      logo: file(relativePath: { eq: "icd-icon.jpg" }) {
+        sharp: childImageSharp {
+          fluid(fit: COVER) {
+            ...GatsbyImageSharpFluid_withWebp_tracedSVG
+          }
+          fixed(fit: COVER) {
+            ...GatsbyImageSharpFixed_withWebp_tracedSVG
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <header className="relative h-40vh">
+      <Img
+        fluid={bgImg.sharp.fluid}
+        className="h-full"
+        alt="header background image"
+      />
+      <div className="absolute inset-0 h-full w-full text-white  flex flex-col items-center justify-center bg-black bg-opacity-50 ">
+        <div className="flex flex-col justify-center items-center space-y-8">
+          <div className="h-36 w-36 ">
+            <Img
+              fluid={logo.sharp.fluid}
+              className="h-full w-full"
+              alt="logo"
+            />
+          </div>
+          <h1 className="font-bold text-2xl tracking-wide font-fira flex items-center leading-relaxed">
+            <span className="text-5xl">I</span>nstitut Congolais de demain
+          </h1>
+        </div>
+        <div className="max-w-3xl  w-full py-6 mt-8 ">
+          <ul className="flex  w-full justify-between text-xs font-thin">
+            <li className="uppercase tracking-widest">Accueil</li>
+            <li className="uppercase tracking-widest">a propos</li>
+            <li className="uppercase tracking-widest">analises</li>
+            <li className="uppercase tracking-widest">podcasts</li>
+            <li className="uppercase tracking-widest">contact</li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div>
-      <h1 className="font-sans">
+    <div className="max-w-2xl mt-16  mx-auto space-y-8">
+      <h1 className=" text-4xl italic ">
         <em>
           "Lorsque nous nous y attendons le moins, la vie nous met au défi de
           tester notre courage et notre volonté de changer ; à un tel moment, il
@@ -19,7 +19,7 @@ const IndexPage = () => (
           permettre de décider si nous acceptons ou non notre destin".
         </em>
       </h1>
-      <h2>- Paulo Coelho,&nbsp;</h2>
+      <h2 className="text-right text-lg">- Paulo Coelho.</h2>
     </div>
   </Layout>
 )
